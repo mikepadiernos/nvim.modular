adceb8d920ca8b39a83169dc3bd3ddccde83bcf0
Michael Padiernos <mike@padiernos.me>
2024-11-01 12:17:40 -0400

    [build] improved blurb for README.md

--- lua/extend/treesitter.lua
+++ lua/extend/treesitter.lua
@@ -1,4 +1,4 @@
 return {
   { enabled = true, name = 'treesitter', 'nvim-treesitter/nvim-treesitter' },
-  { enabled = false, name = 'treesitter-context', 'nvim-treesitter/nvim-treesitter-context' },
+  { enabled = true, name = 'treesitter-context', 'nvim-treesitter/nvim-treesitter-context' },
 }
