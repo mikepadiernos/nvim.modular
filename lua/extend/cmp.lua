e928c52e6326ea1c0201e823bb9583a2280737e1
Michael Padiernos <mike@padiernos.me>
2024-11-07 11:00:46 -0500

    [build] made adjustments to nvim-tree

--- lua/extend/cmp.lua
+++ lua/extend/cmp.lua
@@ -3,6 +3,7 @@ return {
   -- { enabled = true, name = 'cmp', 'hrsh7th/nvim-cmp' },
   -- { enabled = false, name = 'cmp-buffer', 'hrsh7th/cmp-buffer' },
   -- { enabled = false, name = 'cmp-cmdline', 'hrsh7th/cmp-cmdline' },
+  { enabled = false, name = "cmp-emoji", "allaman/emoji.nvim" },
   { enabled = true, name = "cmp-git", "petertriho/cmp-git" },
   { enabled = true, name = "cmp-nerdfonts", "davidmh/cmp-nerdfonts" },
   -- { enabled = false, name = 'cmp-path', 'hrsh7th/cmp-path' },
---
+++ lua/plugins/contrib/nvim-snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    notifier = {
+      style = "minimal",
+      top_down = false,
+    }
+  },
+  config = function(_,opts)
+    require("snacks").setup(opts)
+  end
+}
