abb3404c3a299937e79f9ff2f61ef346e5013675
Michael Padiernos <michael.padiernos@geaerospace.com>
2025-07-25 17:34:27 -0400

    [chore] media & telescope
    - disabled music-controls & remote-sshfs

--- lua/extend/media.lua
+++ lua/extend/media.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    name = "music-controls",
-    "AntonVanAssche/music-controls.nvim",
-    require("plugins.contrib.nvim-music-controls"),
-  },
+  -- {
+  --   name = "music-controls",
+  --   "AntonVanAssche/music-controls.nvim",
+  --   require("plugins.contrib.nvim-music-controls"),
+  -- },
 }
